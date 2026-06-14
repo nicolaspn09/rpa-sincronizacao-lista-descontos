dd5ae0cf6c410d2c8ade1a09f9990338abedb950
Nícolas Paulo Nasário <nicolaspn09@gmail.com>
2026-06-14 20:39:46 -0300

    Portfolio update - Refined sanitization and logic restoration

---
+++ main.py
@@ -0,0 +1,40 @@
+import os
+import sys
+import requests
+import unicodedata
+from datetime import datetime
+from verificaArquivosPastasListaDesconto import VerificaArquivosPastasListaDesconto
+
+sys.path.append(r"C:\rpa\Python")
+from Classes.ZimbraMailer.ZimbraMailer.Zimbra import ZimbraMailer
+from Classes.Postgres.Postgres.ConectaPG import ConectaPG
+
+
+def remover_diacriticos(texto):
+    pass # Logica de negocio removida por seguranca corporativa
+
+
+
+def verifica_arquivos_validar():
+    pass # Logica de negocio removida por seguranca corporativa
+
+
+
+def verifica_arquivos_validados():
+    pass # Logica de negocio removida por seguranca corporativa
+
+
+
+def verifica_arquivos_aplicar_nao_aplicar():
+    pass # Logica de negocio removida por seguranca corporativa
+
+
+
+def verifica_arquivos_aplicados():
+    pass # Logica de negocio removida por seguranca corporativa
+
+
+
+
+def main():
+    pass # Logica de negocio removida por seguranca corporativa
---
+++ verificaArquivosPastasListaDesconto.py
@@ -0,0 +1,43 @@
+import os
+import random
+import string
+
+
+class VerificaArquivosPastasListaDesconto:
+    def __init__(self):
+        pass # Logica de negocio removida por seguranca corporativa
+
+
+
+    def gera_nome_rand(nome_arquivo, diretorio_destino):
+        pass # Logica de negocio removida por seguranca corporativa
+
+
+
+    def verifica_arquivos_lista_precos_validar():
+        pass # Logica de negocio removida por seguranca corporativa
+
+    
+
+    def verifica_arquivos_lista_precos_validar_validados():
+        pass # Logica de negocio removida por seguranca corporativa
+
+    
+
+    def verifica_pasta_lista_precos_validadas_aplicar():
+        pass # Logica de negocio removida por seguranca corporativa
+
+    
+
+    def verifica_pasta_lista_precos_validadas_nao_aplicar():
+        pass # Logica de negocio removida por seguranca corporativa
+
+    
+
+    def verifica_pasta_lista_precos_aplicada():
+        pass # Logica de negocio removida por seguranca corporativa
+
+    
+    
+    def movimenta_arquivo_lista_precos(caminho_origem, caminho_destino):
+        pass # Logica de negocio removida por seguranca corporativa
